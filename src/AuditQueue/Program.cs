c0006bb574f7d4c761f66c27f47ef8aaf9f5222d
Nabin Gurung <ngurung@rekor.ai>
2023-01-23 11:41:24 -0500

    feat: initialize with .net templates

---
+++ src/AuditQueue/Program.cs
@@ -0,0 +1,10 @@
+using AuditQueue;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
